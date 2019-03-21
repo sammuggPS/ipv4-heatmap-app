104df71463eb216c9d631bda8c5e55b113e21e9a
Sam Muggleworth <sam.muggleworth@globant.com>
2019-03-21 04:42:22 -0400

    Move config file into correct spot and get rid of docs directory

--- docs/vue.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  publicPath: '/ipv4-heatmap-app/'
-};
---
+++ vue.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/ipv4-heatmap-app/'
+    : '/'
+};
