426952b3d0289c502c4179bcdd0dff66b68ae5a0
Sam Muggleworth <sam.muggleworth@pointsource.com>
2019-03-21 03:47:19 -0400

    Add mapbox

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="mapbox"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import mapboxgl from 'mapbox-gl';
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  mounted() {
+    mapboxgl.accessToken =
+      'pk.eyJ1Ijoic2FtbXVnZyIsImEiOiJjanRpOWNlZXQwbTRyNGFsZjA2bDAyOXByIn0.SFT7EK3TupAtjvsb2XjBEA';
+    const map = new mapboxgl.Map({
+      container: 'mapbox',
+      style: 'mapbox://styles/mapbox/dark-v9',
+      center: [-98, 40],
+      zoom: 4
+    });
+  },
+  data() {
+    return {};
   }
-}
+};
 </script>
 
-<style>
+<style lang="scss">
+html,
+body,
+#app,
+#mapbox {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#mapbox {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+
+  .mapboxgl-canvas {
+    position: relative !important;
+  }
 }
 </style>
