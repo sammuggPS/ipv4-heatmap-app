9467caa5f5b3c9523018df80533e1bf74de64a0d
Sam Muggleworth <sam.muggleworth@pointsource.com>
2019-03-24 10:00:21 -0400

    Add env configs to hold API URL

--- src/store/actions.js
+++ src/store/actions.js
@@ -8,10 +8,9 @@ import axios from 'axios';
  */
 
  const FETCH_STARTING_RALEIGH_IPS = ({commit}) => {
-   console.log(process.env);
    return axios({
       method: 'get',
-      url: 'http://localhost:5000/ip-coordinates?lowerlong=-78.9100&upperlong=-78.4989&lowerlat=35.6903&upperlat=35.9536'
+      url: `${process.env.VUE_APP_API_URL}/ip-coordinates?lowerlong=-78.9100&upperlong=-78.4989&lowerlat=35.6903&upperlat=35.9536`
     })
     .then((results) => {
       commit('RECEIVED_NEW_FEATURE_COLLECTION', results.data);
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,4 +1,6 @@
 /**
+ * Names of mutation functions should describe the event that caused them.
+ *
  * Mutations expect two parameters
  * @param {*} state - current state from the store created through VueX
  * @param {*} payload - the data with which the state should be updated. All updates must be in one payload
